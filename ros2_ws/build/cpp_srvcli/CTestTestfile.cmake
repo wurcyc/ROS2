51f031cb62db86b7ee05c63e235aa662c4a824f0
wurcyc <726425491@qq.com>
2025-08-17 17:20:13 +0800

    [ROS2}[Jazzy] https://docs.ros.org/en/jazzy/Tutorials/Beginner-Client-Libraries.html 1.Learning Writing a simple publisher and subscriber(C++) 2.Learning Writing a simple service and client(C++)
    Signed-off-by: wurcyc <726425491@qq.com>

---
+++ ros2_ws/build/cpp_srvcli/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/wurc/ros2_ws/src/cpp_srvcli
+# Build directory: /home/wurc/ros2_ws/build/cpp_srvcli
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ ros2_ws/build/turtlesim/turtlesim__py/CMakeFiles/turtlesim__py.dir/cmake_clean.cmake
@@ -0,0 +1,30 @@
+file(REMOVE_RECURSE
+  "/home/wurc/ros2_ws/build/turtlesim/rosidl_generator_py/turtlesim/_turtlesim_s.ep.rosidl_typesupport_c.c"
+  "/home/wurc/ros2_ws/build/turtlesim/rosidl_generator_py/turtlesim/_turtlesim_s.ep.rosidl_typesupport_fastrtps_c.c"
+  "/home/wurc/ros2_ws/build/turtlesim/rosidl_generator_py/turtlesim/_turtlesim_s.ep.rosidl_typesupport_introspection_c.c"
+  "/home/wurc/ros2_ws/build/turtlesim/rosidl_generator_py/turtlesim/action/__init__.py"
+  "/home/wurc/ros2_ws/build/turtlesim/rosidl_generator_py/turtlesim/action/_rotate_absolute.py"
+  "/home/wurc/ros2_ws/build/turtlesim/rosidl_generator_py/turtlesim/action/_rotate_absolute_s.c"
+  "/home/wurc/ros2_ws/build/turtlesim/rosidl_generator_py/turtlesim/msg/__init__.py"
+  "/home/wurc/ros2_ws/build/turtlesim/rosidl_generator_py/turtlesim/msg/_color.py"
+  "/home/wurc/ros2_ws/build/turtlesim/rosidl_generator_py/turtlesim/msg/_color_s.c"
+  "/home/wurc/ros2_ws/build/turtlesim/rosidl_generator_py/turtlesim/msg/_pose.py"
+  "/home/wurc/ros2_ws/build/turtlesim/rosidl_generator_py/turtlesim/msg/_pose_s.c"
+  "/home/wurc/ros2_ws/build/turtlesim/rosidl_generator_py/turtlesim/srv/__init__.py"
+  "/home/wurc/ros2_ws/build/turtlesim/rosidl_generator_py/turtlesim/srv/_kill.py"
+  "/home/wurc/ros2_ws/build/turtlesim/rosidl_generator_py/turtlesim/srv/_kill_s.c"
+  "/home/wurc/ros2_ws/build/turtlesim/rosidl_generator_py/turtlesim/srv/_set_pen.py"
+  "/home/wurc/ros2_ws/build/turtlesim/rosidl_generator_py/turtlesim/srv/_set_pen_s.c"
+  "/home/wurc/ros2_ws/build/turtlesim/rosidl_generator_py/turtlesim/srv/_spawn.py"
+  "/home/wurc/ros2_ws/build/turtlesim/rosidl_generator_py/turtlesim/srv/_spawn_s.c"
+  "/home/wurc/ros2_ws/build/turtlesim/rosidl_generator_py/turtlesim/srv/_teleport_absolute.py"
+  "/home/wurc/ros2_ws/build/turtlesim/rosidl_generator_py/turtlesim/srv/_teleport_absolute_s.c"
+  "/home/wurc/ros2_ws/build/turtlesim/rosidl_generator_py/turtlesim/srv/_teleport_relative.py"
+  "/home/wurc/ros2_ws/build/turtlesim/rosidl_generator_py/turtlesim/srv/_teleport_relative_s.c"
+  "CMakeFiles/turtlesim__py"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/turtlesim__py.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
